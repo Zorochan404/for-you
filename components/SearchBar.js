a7093a6fadbe62083663f8e3d7c163a5fcf2917e
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-20 22:18:55 +0530

    little update

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -23,39 +23,43 @@ const SearchBar = () => {
     const url = `https://beatbump.io/api/v1/search.json?q=${searchText}&filter=songs`;
     console.log(url);
     await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "text/plain",
-        Connection: "keep-alive",
-        "User-Agent": "MY-UA-STRING",
-      },
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/plain',
+            "Connection": "keep-alive",
+            "User-Agent": "MY-UA-STRING"
+        }
     })
-      .then((res) => res.json())
-      .then((apiResponse) => {
-        const formattedResults = apiResponse.results[0].contents.map(
-          (item) => ({
+    .then(res => res.json())
+    .then((apiResponse) => {
+        const formattedResults = apiResponse.results[0].contents.map(item => ({
             id: item.videoId,
-            // url: `https://www.youtube.com/watch?v=${item.videoId}`,
+            playlistId: item.playlistId,
             thumbnail: item.thumbnails[0].url,
             title: item.title,
             uploaderName: item.artistInfo.artist[0].text,
-            duration: item.subtitle[4].text,
-          })
-        );
+            duration: item.subtitle[4].text
+        }));
         setSearchResults(formattedResults);
-        console.log(searchResults);
-      })
-      .catch((error) => {
-        console.error("Error fetching search results:", error);
-      });
-  };
+        
+    })
+    .catch((error) => {
+        console.error('Error fetching search results:', error);
+    });
+};
 
-  const handleItemClick = (item) => {
-    navigation.navigate("song", { songData: item });
-    console.log("Clicked item:", item);
-  };
 
-<<<<<<< HEAD
+
+
+
+
+
+const handleItemClick = (item) => {
+    navigation.navigate('song', { songData: item })
+    console.log('Clicked item:', item);
+};
+
+
   return (
     <View>
       <Text
@@ -116,85 +120,7 @@ const SearchBar = () => {
       </ScrollView>
     </View>
   );
-=======
     
-
-
-    const handleSearch = async () => {
-        const url = `https://beatbump.io/api/v1/search.json?q=${searchText}&filter=songs`;
-        console.log(url);
-        await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'text/plain',
-                "Connection": "keep-alive",
-                "User-Agent": "MY-UA-STRING"
-            }
-        })
-        .then(res => res.json())
-        .then((apiResponse) => {
-            const formattedResults = apiResponse.results[0].contents.map(item => ({
-                id: item.videoId,
-                playlistId: item.playlistId,
-                thumbnail: item.thumbnails[0].url,
-                title: item.title,
-                uploaderName: item.artistInfo.artist[0].text,
-                duration: item.subtitle[4].text
-            }));
-            setSearchResults(formattedResults);
-            
-        })
-        .catch((error) => {
-            console.error('Error fetching search results:', error);
-        });
-    };
-    
-
-    
-
-    
-    
-
-    const handleItemClick = (item) => {
-        navigation.navigate('song', { songData: item })
-        console.log('Clicked item:', item);
-    };
-
-    return (
-        
-        <View >
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-                <Text>ForU</Text>
-                <TextInput
-                    placeholder='Search music'
-                    keyboardType='default'
-                    value={searchText}
-                    onChangeText={text => setSearchText(text)}
-                    style={{ flex: 1, marginLeft: 10 }}
-                />
-                <TouchableOpacity
-                    onPress={handleSearch}
-                ><Text>Search</Text></TouchableOpacity>
-            </View>
-            <ScrollView contentContainerStyle={{paddingBottom: 50, marginHorizontal: 15}}>
-             {searchResults?.map((song) => (
-                    
-                        <SearchedSong
-                            key={song.id}
-                            id={song.id}
-                            playlistId={song.playlistId}
-                            thumbnail={song.thumbnail}
-                            title={song.title}
-                            uploaderName={song.uploaderName}
-                            // uploaderUrl={song.uploaderUrl}
-                            duration={song.duration}
-                            onPress={() => handleItemClick(song)}
-                        />
-                ))}
-            </ScrollView>
-        </View>
-    );
->>>>>>> 321f0d676d1521eb82cfaf6368f36bda0c873422
 };
 
 export default SearchBar;
